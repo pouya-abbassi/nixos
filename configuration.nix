e9f30fe3fb5fb8d8bacd05b13171693c17e371b0
Pouya Abbassi <abbassi.pouya@gmail.com>
2024-05-04 13:35:32 +0330

    Switch to GDM
    remove KDE comment

--- configuration.nix
+++ configuration.nix
@@ -75,9 +75,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
 
   programs.hyprland = {
     enable = true;
