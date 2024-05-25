30175a08790998fb7a18bacaa4b9447c3d9cc0a0
Pouya Abbassi <abbassi.pouya@gmail.com>
2024-05-26 00:19:08 +0330

    Sort and group applicotions

--- home.nix
+++ home.nix
@@ -13,91 +13,103 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+
+    # utils
     ack
     aria2
-    ark
-    blender-hip
     btop
-    calibre
-    clang-tools
-    clojure
-    clojure-lsp
-    darktable
     dig
-    dolphin
-    dunst
     encfs
     ffmpeg
-    fg42.outputs.packages.x86_64-linux.default
     file
-    firefox
-    gimp
-    git
-    gnome.gnome-disk-utility
-    go
-    gparted
-    graphite-kde-theme
     hdparm
     htop
-    hyprland
-    hyprlock
-    hyprpaper
-    hyprshot
-    inkscape
-    kate
-    ledger-live-desktop
-    libnotify
-    librewolf
-    libsForQt5.filelight
-    libsForQt5.qt5ct
-    libsForQt5.skanlite
-    lxappearance
-    lxappearance
     netcat
-    networkmanagerapplet
     nix-tree
-    nodePackages.svgo
-    obs-studio
     parted
     pass
-    pavucontrol
-    pcmanfm
     pinentry-curses
-    protonmail-bridge-gui
     protonvpn-cli
-    protonvpn-gui
     ripgrep
-    rofi-wayland
-    siril
-    slack
     smartmontools
-    spotify
     sshfs
-    sublime
-    swayidle
-    sxiv
-    telegram-desktop
-    terminator
-    terraform
     thefuck
-    thunderbird
-    tig
     tldr
     tmux
     traceroute
     tree
     udiskie
     udisks
-    ungoogled-chromium
     unzip
     v2raya
-    ventoy
-    vim
-    vlc
     wget
-    wl-clipboard
-    zafiro-icons
     zip
+
+    # desktop
+    dunst
+    hyprland
+    hyprlock
+    hyprpaper
+    hyprshot
+    libnotify
+    networkmanagerapplet
+    rofi-wayland
+    swayidle
+    wl-clipboard
+
+    # gui
+    ark
+    blender-hip
+    calibre
+    darktable
+    dolphin
+    gimp
+    gnome.gnome-disk-utility
+    gparted
+    graphite-kde-theme
+    inkscape
+    ledger-live-desktop
+    libreoffice
+    libsForQt5.filelight
+    libsForQt5.qt5ct
+    libsForQt5.skanlite
+    loupe
+    lxappearance
+    obs-studio
+    pavucontrol
+    pcmanfm
+    protonmail-bridge-gui
+    protonvpn-gui
+    siril
+    slack
+    spotify
+    sublime
+    telegram-desktop
+    terminator
+    thunderbird
+    ventoy
+    vlc
+    zafiro-icons
+
+    # browsers
+    firefox
+    librewolf
+    ungoogled-chromium
+
+    # code
+    ansible
+    awscli2
+    clang-tools
+    clojure
+    clojure-lsp
+    fg42.outputs.packages.x86_64-linux.default
+    git
+    nodePackages.svgo
+    nodejs_22
+    terraform
+    tig
+    vim
+    virtualenv
   ];
 
   programs.emacs = {
