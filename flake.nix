ac6ed1f449ca2541900cf275d547d2d6c05280b1
Pouya Abbassi <abbassi.pouya@gmail.com>
2024-06-30 22:22:34 +0330

    Pin flake inputs
    Pin fg42 input

--- flake.nix
+++ flake.nix
@@ -2,15 +2,10 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-    };
-    fg42.url = "git+https://devheroes.codes/pouya-abbassi/FG42";
+    nixpkgs.url = "github:nixos/nixpkgs/c5c98cfc7060e89443cb0162412d9f45d992c81a";
+    home-manager.url = "github:nix-community/home-manager/36317d4d38887f7629876b0e43c8d9593c5cc48d";
+    stylix.url = "github:danth/stylix/1ff9d37d27377bfe8994c24a8d6c6c1734ffa116";
+    fg42.url = "git+https://devheroes.codes/FG42/FG42?rev=36ea7f031c10539c66ff15739a0a97e8c1d10463";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, stylix, fg42, ... }: {
