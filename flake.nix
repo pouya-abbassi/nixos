e1806306a1b5f4a604f1eb1b24ee1d89685401d8
Pouya Abbassi <abbassi.pouya@gmail.com>
2024-10-13 22:59:43 +0330

    Use nixos-unstable branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,6 +11,7 @@
 
     stylix = {
       url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     fg42 = {
