ae4370c8317c58a57f851310208cfec2e0487bde
Pouya Abbassi <abbassi.pouya@gmail.com>
2024-10-19 17:09:46 +0330

    Add agenix + noti

---
+++ secrets/secrets.nix
@@ -0,0 +1,8 @@
+let
+  pouya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILyvUJK9jI07Vp1jBNUvlOYjX2/QQy+gFtotGtNko4vt pouya@strix";
+  users = [ pouya ];
+
+in
+{
+  "noti.age".publicKeys = [ pouya ];
+}
