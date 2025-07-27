810598fa6b7daff90ac6cb71bf09dcf75b6230d7
Pouya Abbassi <abbassi.pouya@gmail.com>
2025-07-27 16:30:30 +0330

    switch to latest kernel

--- strix/hardware-configuration.nix
+++ strix/hardware-configuration.nix
@@ -8,6 +8,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
